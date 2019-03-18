e178e27fc57c0a169ff344f52ee9c5cd4bdb94d8
D070429 <christian.gunzelmann@sap.com>
2019-03-18 18:39:22 +0100

    Neues Datenmodell

---
+++ src/main/java/dhbwka/wwi/vertsys/javaee/jmovie/tasks/jpa/Genere.java
@@ -0,0 +1,63 @@
+/*
+ * Copyright © 2019 Dennis Schulmeister-Zimolong
+ * 
+ * E-Mail: dhbw@windows3.de
+ * Webseite: https://www.wpvs.de/
+ * 
+ * Dieser Quellcode ist lizenziert unter einer
+ * Creative Commons Namensnennung 4.0 International Lizenz.
+ */
+package dhbwka.wwi.vertsys.javaee.jmovie.tasks.jpa;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author D070429
+ */
+@Entity
+public class Genere implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Genere)) {
+            return false;
+        }
+        Genere other = (Genere) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "dhbwka.wwi.vertsys.javaee.jmovie.tasks.jpa.Genere[ id=" + id + " ]";
+    }
+    
+}
