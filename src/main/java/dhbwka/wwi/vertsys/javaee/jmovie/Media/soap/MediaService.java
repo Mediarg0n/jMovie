be4b692e45c4ba760f3cb691ef1413c73a74f0af
Björn <b.pallutt>
2019-03-27 14:05:57 +0100

    Revert "getAllMedia SOAP"
    This reverts commit c7f2675c1d9d9aa7c49617e8b722771a4d26de97.

--- src/main/java/dhbwka/wwi/vertsys/javaee/jmovie/Media/soap/MediaService.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/jmovie/Media/soap/MediaService.java
@@ -10,9 +10,7 @@
 package dhbwka.wwi.vertsys.javaee.jmovie.Media.soap;
 
 import dhbwka.wwi.vertsys.javaee.jmovie.Media.ejb.MediaBean;
-import dhbwka.wwi.vertsys.javaee.jmovie.Media.jpa.Media;
 import dhbwka.wwi.vertsys.javaee.jmovie.common.ejb.UserBean;
-import java.util.List;
 import javax.ejb.EJB;
 import javax.jws.WebService;
 import javax.jws.WebMethod;
@@ -34,13 +32,13 @@ public class MediaService {
     
     @WebMethod
     @WebResult(name = "Medien")
-    public List<Media> getAllMedia(
+    public String getSecretMessage(
             @WebParam(name = "username", header = true) String username,
-            @WebParam(name = "password", header = true) String password) throws UserBean.InvalidCredentialsException
+            @WebParam(name = "password", header = true) String password)
             {
-                
-                userBean.checkPassword(username, password);    
-                return mediaBean.search(null,null,null);
+
+        
+        return "Streng geheim! For your eyes only!";
     }
 
 }
