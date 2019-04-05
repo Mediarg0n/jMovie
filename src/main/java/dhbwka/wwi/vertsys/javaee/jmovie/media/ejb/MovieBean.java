ca745e04dc2381225663ef4785539ee366469c0f
D070429 <christian.gunzelmann@sap.com>
2019-04-05 22:12:06 +0200

    netbeans addet nicht alles

--- src/main/java/dhbwka/wwi/vertsys/javaee/jmovie/media/ejb/MovieBean.java
+++ src/main/java/dhbwka/wwi/vertsys/javaee/jmovie/media/ejb/MovieBean.java
@@ -10,9 +10,16 @@
 package dhbwka.wwi.vertsys.javaee.jmovie.media.ejb;
 
 import dhbwka.wwi.vertsys.javaee.jmovie.common.ejb.EntityBean;
+import dhbwka.wwi.vertsys.javaee.jmovie.media.jpa.Genre;
 import dhbwka.wwi.vertsys.javaee.jmovie.media.jpa.Movie;
+import dhbwka.wwi.vertsys.javaee.jmovie.media.jpa.WatchStatus;
+import java.util.List;
 import javax.annotation.security.RolesAllowed;
 import javax.ejb.Stateless;
+import javax.persistence.criteria.CriteriaBuilder;
+import javax.persistence.criteria.CriteriaQuery;
+import javax.persistence.criteria.Predicate;
+import javax.persistence.criteria.Root;
 
 
 /**
@@ -25,6 +32,42 @@ public class MovieBean extends EntityBean<Movie, Long> {
     
      public MovieBean() {
         super(Movie.class);
+    }
+     
+     
+     public List<Movie> search(String title, Genre genre, WatchStatus status) {
+        // Hilfsobjekt zum Bauen des Query
+        CriteriaBuilder cb = this.em.getCriteriaBuilder();
+        
+        // SELECT t FROM Serie s
+        CriteriaQuery<Movie> query = cb.createQuery(Movie.class);
+        Root<Movie> from = query.from(Movie.class);
+        query.select(from);
+
+        // ORDER BY dueDate, dueTime
+        query.orderBy(cb.desc(from.get("releaseDate")));
+        
+        // WHERE s.title LIKE :title
+        Predicate p = cb.conjunction();
+        
+        if (title != null && !title.trim().isEmpty()) {
+            p = cb.and(p, cb.like(from.get("title"), "%" + title + "%"));
+            query.where(p);
+        }
+        
+        // WHERE s.genre = :genre
+        if (genre != null) {
+            p = cb.and(p, cb.isMember(genre,from.get("genres")));
+            query.where(p);
+        }
+        
+        // WHERE s.status = :status
+        if (status != null) {
+            p = cb.and(p, cb.equal(from.get("status"), status));
+            query.where(p);
+        }
+        
+        return em.createQuery(query).getResultList();
     }
      
 }
